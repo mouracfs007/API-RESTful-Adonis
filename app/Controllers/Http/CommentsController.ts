50f9e1b7938ac39f45676dbce73d8514e598d857
Mouracfs <danielmourajuve10@gmail.com>
2023-08-29 20:45:51 -0300

    API-RESTful: ok

---
+++ app/Controllers/Http/CommentsController.ts
@@ -0,0 +1,20 @@
+import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import Moment from '../../Models/Moment'
+import Comment from '../../Models/Comment'
+
+export default class CommentsController {
+  public async store({ request, params, response }: HttpContextContract) {
+    const body = request.body()
+    const momentId = params.momentId
+
+    await Moment.findOrFail(momentId)
+    body.momentId = momentId
+    const comment = await Comment.create(body)
+    response.status(201)
+
+    return {
+      message: 'Comentário adicionado com sucesso!',
+      data: comment,
+    }
+  }
+}
